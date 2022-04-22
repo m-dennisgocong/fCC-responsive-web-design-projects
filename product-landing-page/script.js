1088b128f541eb627ae90915c29f4ecd1aa44805
Dennis Goc-ong <m.dennisgoc-ong>
2022-04-22 20:10:10 +0700

    :sparkles: product-landing-page
    Added a script for mobile menu

---
+++ product-landing-page/script.js
@@ -0,0 +1,9 @@
+const menu = document.querySelector('#menu');
+const close = document.querySelector('#close');
+const nav = document.querySelector('nav').classList;
+menu.addEventListener('click', () => {
+  nav.add('nav-bar-open');
+})
+close.addEventListener('click', () => {
+  nav.remove('nav-bar-open');
+})

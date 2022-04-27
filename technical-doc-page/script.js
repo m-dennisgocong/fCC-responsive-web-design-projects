1463dc96b627fb011efd8611d7489f378836f8c2
Dennis Goc-ong <m.dennisgoc-ong>
2022-04-27 18:54:53 +0700

    Done: Mobile design

--- technical-doc-page/script.js
+++ technical-doc-page/script.js
@@ -2,8 +2,8 @@ const menu = document.querySelector('#menu');
 const close = document.querySelector('#close');
 const nav = document.querySelector('.nav-menu').classList;
 menu.addEventListener('click', () => {
-   nav.add('nav-bar-open');
+  nav.add('nav-bar-open');
 })
 close.addEventListener('click', () => {
-   nav.remove('nav-bar-open');
+  nav.remove('nav-bar-open');
 })
